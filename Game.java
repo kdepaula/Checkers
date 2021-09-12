771db5f86ab2f16ae4def18c0d0c2a406d219f59
kdepaula <42978472+kdepaula@users.noreply.github.com>
2021-09-12 13:07:42 -0400

    Update Game.java

--- Game.java
+++ Game.java
@@ -1,31 +1,9 @@
-/**
- * CIS 120 HW09 - TicTacToe Demo
- * (c) University of Pennsylvania
- * Created by Bayley Tuch, Sabrina Green, and Nicolas Corona in Fall 2020.
- */
-
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
 
-/**
- * This class sets up the top-level frame and widgets for the GUI.
- * 
- * This game adheres to a Model-View-Controller design framework.  This
- * framework is very effective for turn-based games.  We STRONGLY 
- * recommend you review these lecture slides, starting at slide 8, 
- * for more details on Model-View-Controller:  
- * https://www.seas.upenn.edu/~cis120/current/files/slides/lec37.pdf
- * 
- * In a Model-View-Controller framework, Game initializes the view,
- * implements a bit of controller functionality through the reset
- * button, and then instantiates a GameBoard.  The GameBoard will
- * handle the rest of the game's view and controller functionality, and
- * it will instantiate a TicTacToe object to serve as the game's model.
- */
 public class Game implements Runnable {
     public void run() {
-        // NOTE: the 'final' keyword denotes immutability even for local variables.
 
         // Top-level frame in which game components live
         final JFrame frame = new JFrame("Draughts");
@@ -145,7 +123,7 @@ public class Game implements Runnable {
 
     /**
      * Main method run to start and run the game. Initializes the GUI elements specified in Game and
-     * runs it. IMPORTANT: Do NOT delete! You MUST include this in your final submission.
+     * runs it. 
      */
     public static void main(String[] args) {
         SwingUtilities.invokeLater(new Game());
